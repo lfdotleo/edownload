7bc0ba8a85ecdcf72e36d8d9ee708d437b3584a8
liufei <liufei@mkit.com.cn>
2020-08-24 21:36:22 +0800

    update

--- edownload.sh
+++ edownload.sh
@@ -5,8 +5,11 @@
 out_dir="./out"
 mkdir $out_dir
 referer=$1
+referer=$(echo $referer | sed "s/\\\\//g")
 m3u8_url=$2
+m3u8_url=$(echo $m3u8_url | sed "s/\\\\//g")
 ts_url=$3
+ts_url=$(echo $ts_url | sed "s/\\\\//g")
 file_name=$4
 file_name=${file_name// /}
 m3u8_file="$file_name.m3u8"
@@ -18,7 +21,6 @@ wget -E --referer "$referer" -r -m "$m3u8_url" -O "$m3u8_file"
 
 start=0
 end=$(tail -2 $m3u8_file | grep 'end' | awk -F 'end=|&type' '{print $2}')
-ts_url=$(echo $ts_url | sed "s/\\\\//g")
 ts_url=$(echo $ts_url | sed "s/\(start=\).*\(&end\)/\1${start}\2/g")
 ts_url=$(echo $ts_url | sed "s/\(end=\).*\(&type\)/\1${end}\2/g")
 wget -E --referer "$referer" -r -m "$ts_url" -O "$ts_file"
